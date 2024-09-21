0f6f645e4d6656639d429925fe1a55705c0290c9
Erick Coutinho <erickcoutinho08@hotmail.com>
2024-09-21 13:33:55 -0300

    Removendo CORS

--- api/app.js
+++ api/app.js
@@ -1,6 +1,5 @@
 require ('dotenv').config()
 const express = require ('express')
-const cors = require('cors')
 const mongoose = require ('mongoose')
 const bcrypt = require ('bcryptjs')
 const jwt = require('jsonwebtoken')
